aefe6efdebd44f7be5c196a13cb17d208457e8d8
harsh <hvbadeiya24@gmail.com>
2022-05-01 12:46:14 +0530

    done with kotlin upgrade

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/harsh/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/harsh/StudioProjects/flash-chat-flutter"
+export "FLUTTER_APPLICATION_PATH=/home/harsh/Documents/git-flash-chat/Flash-Chat"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
@@ -6,7 +8,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = '/chatloginScreen';
-
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -19,12 +20,10 @@ class _ChatScreenState extends State<ChatScreen> {
 
   String messageText;
   var messages1;
-  final CollectionReference  = FirebaseFirestore.instance.collection('messages');
-  String sender="";
-  String text="";
-
-
-
+  final CollectionReference =
+  FirebaseFirestore.instance.collection('messages').doc('text');
+  String sender = "";
+  String text = "";
 
   @override
   //INIT
@@ -33,7 +32,6 @@ class _ChatScreenState extends State<ChatScreen> {
 
     getCurrentUser();
   }
-
 
   void getCurrentUser() async {
     try {
@@ -47,7 +45,7 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-   getMessages() async {
+  getMessages() async {
     final messages = await _firestore.collection('messages').get();
 
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
@@ -57,16 +55,17 @@ class _ChatScreenState extends State<ChatScreen> {
     }
     return messages;
   }
-  showMessage(){
 
-    Map<String,String> msg = {
+  showMessage() {
+    Map<String, String> msg = {
       "sender": sender,
       "text": text,
-
     };
   }
-
-
+  // static User fromJson(Map<String,dynamic> json)=> message(
+  //   sender:json['sender'],
+  //   text:json['text'],
+  // );
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,52 +90,56 @@ class _ChatScreenState extends State<ChatScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
-              child: StreamBuilder<QuerySnapshot>(stream: _firestore.collection('messages').snapshots(),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('messages').snapshots(),
                 builder: (context, snapshot) {
-                  if (snapshot.hasError){
-                    return Text('Something went wrong');
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading');
+                  } else {
+                    final messages = snapshot.data?.docs;
 
-                  } else if(snapshot.hasData|| snapshot.data!=null) {
-                     final messages = snapshot.data?.docs;
-                  ListView.builder(itemBuilder: (BuildContext context, int index){
+                    return ListView.builder(
+                        itemCount: messages.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          // print(jsonDecode(messages[index].data()));
+                          // print();
+                          var message =
+                          jsonDecode(jsonEncode(messages[index].data()));
+                          return Dismissible(
+                              key: Key(index.toString()),
+                              child: Card(
+                                elevation: 4,
+                                child: ListTile(
+                                  title: Text(message["sender"] == null
+                                      ? "Null"
+                                      : message["sender"]),
+                                  subtitle:
+                                  Text(message["text"]==null ? "NULL":message["text"] ),
+                                ),
+                              ));
+                          // return Column(
+                          //   children: [
+                          //     ListView(
+                          //       children: [Text(message["sender"] == null
+                          //               ? "Null"
+                          //               : message["sender"]),
+                          //       Text(message["text"]==null ? "NULL":message["text"]) ],
+                          //     )
+                          //   ],
+                          // );
+                        });
 
-                   
-                    QueryDocumentSnapshot<Object> documentSnapshot =
-                    snapshot.data?.docs[index];
-                    return Dismissible(key: Key(index.toString()),
-                        child: Card(
-                      elevation: 4,
-                      child: ListTile(
+                    List<Text> messageWidgets = [];
+                    for (var message in messages) { final messageText = message["text"];
+                    final messageSender = message["sender"];
+                    final messageWidget = Text('$messageText from $messageSender');
+                    messageWidgets.add(messageWidget);
+                    }
 
-                        title: Text((messages != null) ? (messages[index]["sender"]):""),
-                        subtitle: Text("papa"),
-                      ) ,
-                    ));
-                  });
-
-
-                    // List<Text> messageWidgets = [];
-                    // for (var message in messages) { final messageText = message["text"];
-                    // final messageSender = message["sender"];
-                    // final messageWidget = Text('$messageText from $messageSender');
-                    // messageWidgets.add(messageWidget);
-                    // }
-
-
-                    // return Column( children: messageWidgets, );
+                    return Column( children: messageWidgets, );
                   }
-
-
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        Colors.black,
-                      ),
-                    ),
-                  );
-
-
-                },),
+                },
+              ),
             ),
             Container(
               decoration: kMessageContainerDecoration,
@@ -214,5 +217,223 @@ class _ChatScreenState extends State<ChatScreen> {
 // }
 //${snapshot.data[index].sender}
 
-
-
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flash_chat/constants.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+//
+// class ChatScreen extends StatefulWidget {
+//   static const String id = '/chatloginScreen';
+//
+//
+//   @override
+//   _ChatScreenState createState() => _ChatScreenState();
+// }
+//
+// class _ChatScreenState extends State<ChatScreen> {
+//   final _firestore = FirebaseFirestore.instance;
+//   final _auth = FirebaseAuth.instance;
+//   User loggedInUser;
+//
+//   String messageText;
+//   var messages1;
+//   final CollectionReference  = FirebaseFirestore.instance.collection('messages');
+//   String sender="";
+//   String text="";
+//
+//
+//
+//
+//   @override
+//   //INIT
+//   void initState() {
+//     super.initState();
+//
+//     getCurrentUser();
+//   }
+//
+//
+//   void getCurrentUser() async {
+//     try {
+//       final user = await _auth.currentUser;
+//       if (user != null) {
+//         loggedInUser = user;
+//         print(loggedInUser.email);
+//       }
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+//
+//    getMessages() async {
+//     final messages = await _firestore.collection('messages').get();
+//
+//     await for (var snapshot in _firestore.collection('messages').snapshots()) {
+//       for (var message in snapshot.docs) {
+//         print(message.data());
+//       }
+//     }
+//     return messages;
+//   }
+//   showMessage(){
+//
+//     Map<String,String> msg = {
+//       "sender": sender,
+//       "text": text,
+//
+//     };
+//   }
+//
+//
+//
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         leading: null,
+//         actions: <Widget>[
+//           IconButton(
+//               icon: Icon(Icons.close),
+//               onPressed: () {
+//                 // _auth.signOut();
+//                 // Navigator.pop(context);
+//                 getMessages();
+//               }),
+//         ],
+//         title: Text('⚡️Chat'),
+//         backgroundColor: Colors.lightBlueAccent,
+//       ),
+//       body: SafeArea(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             Expanded(
+//               child: StreamBuilder<QuerySnapshot>(stream: _firestore.collection('messages').snapshots(),
+//                 builder: (context, snapshot) {
+//                   if (snapshot.hasError){
+//                     return Text('Something went wrong');
+//
+//                   } else if(!snapshot.hasData|| snapshot.data!=null) {
+//                     final messages = snapshot.data?.docs;
+//                   // ListView.builder(itemBuilder: (BuildContext context, int index){
+//                   //
+//                   //   final messages = snapshot.data?.docs;
+//                   //   QueryDocumentSnapshot<Object> documentSnapshot =
+//                   //   snapshot.data?.docs[index];
+//                   //   return Dismissible(key: Key(index.toString()),
+//                   //       child: Card(
+//                   //     elevation: 4,
+//                   //     child: ListTile(
+//                   //
+//                   //       title: Text((messages != null) ? (messages[index]["sender"]):""),
+//                   //       subtitle: Text("papa"),
+//                   //     ) ,
+//                   //   ));
+//                   // });
+//
+//
+//                     List<Text> messageWidgets = [];
+//                     for (var message in messages) { final messageText = message["text"];
+//                     final messageSender = message["sender"];
+//                     final messageWidget = Text('$messageText from $messageSender');
+//                     messageWidgets.add(messageWidget);
+//                     }
+//
+//
+//                     return Column( children: [
+//                       ListTile( ),
+//                     ], );
+//                   }
+//
+//
+//                   return const Center(
+//                     child: CircularProgressIndicator(
+//                       valueColor: AlwaysStoppedAnimation<Color>(
+//                         Colors.black,
+//                       ),
+//                     ),
+//                   );
+//
+//
+//                 },),
+//             ),
+//             Container(
+//               decoration: kMessageContainerDecoration,
+//               child: Row(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: <Widget>[
+//                   Expanded(
+//                     child: TextField(
+//                       onChanged: (value) {
+//                         messageText = value;
+//                       },
+//                       decoration: kMessageTextFieldDecoration,
+//                     ),
+//                   ),
+//                   TextButton(
+//                     onPressed: () {
+//                       _firestore.collection('messages').add({
+//                         'text': messageText,
+//                         'sender': loggedInUser.email,
+//                       });
+//                     },
+//                     child: Text(
+//                       'Send',
+//                       style: kSendButtonTextStyle,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// //     if (snapshot.hasData)
+// //     { final messages = _firestore
+// //         .collection('message')
+// //         .snapshots();
+// //
+// //     print(messages);
+// //
+// //
+// //
+// //     // for (var message in messages) {
+// //     // }
+// //     // return Column( children: messageWidgets, );
+// //
+// //
+// //     children: snapshot.data.docs.map((DocumentSnapshot document) {
+// //
+// //     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+// //
+// //     final messageText = data['text'];
+// //     final messageSender = data['sender'];
+// //
+// //     return
+// //     ListTile(
+// //     textColor: Colors.black,
+// //     title: Text(messageText),
+// //     subtitle: Text(messageSender),
+// //     );
+// //
+// //
+// //     }
+// //     ).toList();
+// //     } else { return Text('No Data Found');
+// //     }
+// //
+// //   }
+// //   catch(e){print(e);
+// //   }
+// //   return Container();
+// // }
+// //${snapshot.data[index].sender}
+//
+//
+//
